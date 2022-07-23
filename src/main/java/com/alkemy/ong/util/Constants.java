5c55afa686ce5c0c332b8d241538dd24e09bfb8d
Juan Ceron <juanceron256@gmail.com>
2022-07-23 15:49:37 -0500

    added SendGrid Dynamic Template integration in MailService and realted clases

--- src/main/java/com/alkemy/ong/util/Constants.java
+++ src/main/java/com/alkemy/ong/util/Constants.java
@@ -7,4 +7,7 @@ public class Constants {
     public static final String SEND_GRID_WELCOME = "Welcome";
     public static final String SEND_GRID_TYPE = "text/plain";
     public static final String SEND_GRID_MESSAGE = "Welcome to the API with Spring Boot by Alkymers Java";
+    public static final String SEND_GRID_TEMPLATE_TITLE = "Bienvenido a Somos Mas!";
+    public static final String SEND_GRID_TEMPLATE_MESSAGE = "Registro exitoso!";
+
 }
