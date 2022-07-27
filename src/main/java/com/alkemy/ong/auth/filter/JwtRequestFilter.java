ee94e3c70084c787c6caad49366bbbe98b2da425
Maxi Falcone <maxifop96@gmail.com>
2022-07-27 00:05:56 -0300

    Comment deleted on auth/filter/JwtFilter

--- src/main/java/com/alkemy/ong/auth/filter/JwtRequestFilter.java
+++ src/main/java/com/alkemy/ong/auth/filter/JwtRequestFilter.java
@@ -49,7 +49,6 @@ public class JwtRequestFilter extends OncePerRequestFilter {
                 UsernamePasswordAuthenticationToken authReq =
                         new UsernamePasswordAuthenticationToken(userDetails.getUsername(), userDetails.getPassword(), userDetails.getAuthorities());
                 authReq.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-//                Authentication auth = authenticationManager.authenticate(authReq);
                 SecurityContextHolder.getContext().setAuthentication(authReq);
             }
 
