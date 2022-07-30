db0cf15a48fcc982f37c3f836f638a0be2aff539
emirmoretti <emir.moretti@hotmail.com.ar>
2022-07-30 14:00:19 -0300

    member controller

--- src/main/java/com/alkemy/ong/controller/MemberController.java
+++ src/main/java/com/alkemy/ong/controller/MemberController.java
@@ -1,6 +1,8 @@
 package com.alkemy.ong.controller;
 
+import com.alkemy.ong.dto.MemberDto;
 import com.alkemy.ong.exception.ApiError;
+import com.alkemy.ong.service.impl.MemberServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,7 +23,7 @@ public class MemberController {
     @PostMapping
     public ResponseEntity<?> create(@Valid @RequestBody MemberDto memberDto){
         try {
-            return ResponseEntity.status(HttpStatus.CREATED).body(memberServiceImpl.createMember(memberDto));
+            return ResponseEntity.status(HttpStatus.CREATED).body(memberServiceImpl.create(memberDto));
         } catch (Exception e){
             throw new ApiError(HttpStatus.BAD_REQUEST, e);
         }
