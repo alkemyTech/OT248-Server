16d473bb76c344c54642aa406db7e927fcea374b
emirmoretti <emir.moretti@hotmail.com.ar>
2022-08-02 16:56:48 -0300

    deletedById method impl

--- src/main/java/com/alkemy/ong/service/impl/NewsServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/NewsServiceImpl.java
@@ -6,9 +6,13 @@ import com.alkemy.ong.model.News;
 import com.alkemy.ong.repository.NewsRepository;
 import com.alkemy.ong.service.NewsService;
 import com.alkemy.ong.service.mapper.NewsMapper;
+import javassist.NotFoundException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class NewsServiceImpl implements NewsService {
@@ -29,6 +33,19 @@ public class NewsServiceImpl implements NewsService {
 
     public NewsDto findNewsById(Long id) {
         return newsMapper.newsEntityToDTO(newsRepository.findById(id).get());
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(Long id) throws Exception{
+        try {
+            Optional<News> news = newsRepository.findById(id);
+            if(news.isPresent()){
+                newsRepository.deleteById(id);
+            }
+        } catch (Exception e){
+            throw new NotFoundException("news not found");
+        }
     }
 
 
