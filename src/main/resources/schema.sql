20a90b489139694028285f109239759817efa91f
Jonathan Ramirez <52109489+JonathanRamirez1@users.noreply.github.com>
2022-08-01 20:18:21 -0500

    #OT248-52 - Se modificó el Seed Data

--- src/main/resources/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE activities (
-    id LONG PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    name VARCHAR NOT NULL,
-    content VARCHAR NOT NULL,
-    image VARCHAR NOT NULL,
-    createAt DATE,
-    updateAt DATE,
-    deleted BOOLEAN
-);
