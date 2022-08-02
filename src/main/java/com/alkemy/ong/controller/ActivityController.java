b5d0f84aebfe2570167d1344954aebffb11619b3
Maxi <maxifop96@gmail.com>
2022-08-02 13:21:46 -0300

    restore application properties and uncomment ActivityController

--- src/main/java/com/alkemy/ong/controller/ActivityController.java
+++ src/main/java/com/alkemy/ong/controller/ActivityController.java
@@ -1,30 +1,30 @@
-//package com.alkemy.ong.controller;
-//
-//import com.alkemy.ong.exception.ApiError;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//import javax.validation.Valid;
-//
-//@RestController
-//@RequestMapping
-//public class ActivityController {
-//
-//    @Autowired
-//    private ActivityService activityService;
-//
-//    @PostMapping("/activities")
-//    public ResponseEntity<?> registerActivities(@Valid @RequestBody ActivityDto activityDto) {
-//        try {
-//            return ResponseEntity
-//                    .status(HttpStatus.CREATED)
-//                    .body(activityService.saveActivity(activityDto));
-//        } catch (Exception exception) {
-//            throw new ApiError(HttpStatus.BAD_REQUEST, exception);
-//        }
-//    }
-//}
+package com.alkemy.ong.controller;
+
+import com.alkemy.ong.exception.ApiError;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping
+public class ActivityController {
+
+    @Autowired
+    private ActivityService activityService;
+
+    @PostMapping("/activities")
+    public ResponseEntity<?> registerActivities(@Valid @RequestBody ActivityDto activityDto) {
+        try {
+            return ResponseEntity
+                    .status(HttpStatus.CREATED)
+                    .body(activityService.saveActivity(activityDto));
+        } catch (Exception exception) {
+            throw new ApiError(HttpStatus.BAD_REQUEST, exception);
+        }
+    }
+}
