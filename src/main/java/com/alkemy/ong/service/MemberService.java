bb4b3f8c4e4faf656da73afb152280b1d86b9eb7
emirmoretti <emir.moretti@hotmail.com.ar>
2022-08-03 21:20:00 -0300

    getAll method

--- src/main/java/com/alkemy/ong/service/MemberService.java
+++ src/main/java/com/alkemy/ong/service/MemberService.java
@@ -2,8 +2,11 @@ package com.alkemy.ong.service;
 
 import com.alkemy.ong.dto.MemberDto;
 
+import java.util.List;
+
 public interface MemberService {
 
     public MemberDto create(MemberDto memberDto);
+    List<MemberDto> getAll();
 
 }
