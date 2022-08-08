3d3c07b75f7e1271c410aba3d680a621edc623a9
emirmoretti <emir.moretti@hotmail.com.ar>
2022-08-08 14:54:08 -0300

    deleted method added

--- src/main/java/com/alkemy/ong/controller/SlideController.java
+++ src/main/java/com/alkemy/ong/controller/SlideController.java
@@ -3,8 +3,12 @@ package com.alkemy.ong.controller;
 import com.alkemy.ong.dto.response.SlideResponseDTO;
 import com.alkemy.ong.service.SlideService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 @RequestMapping("/slides")
@@ -13,6 +17,9 @@ public class SlideController {
     @Autowired
     private SlideService slideService;
 
+    @Autowired
+    private MessageSource messageSource;
+
     @GetMapping("/{id}")
     public ResponseEntity<SlideResponseDTO> getById (@PathVariable Long id){
             SlideResponseDTO slideResponse = slideService.getById(id);
@@ -20,7 +27,11 @@ public class SlideController {
     }
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteSlide(@PathVariable(name = "id") Long id){
-
-        return null;
+        try {
+            slideService.deleteById(id);
+        } catch (Exception e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(messageSource.getMessage("slides.deleted.message", null, Locale.US), HttpStatus.OK);
     }
 }
