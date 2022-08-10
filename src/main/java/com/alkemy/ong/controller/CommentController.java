1f59e89c2a0da0626da038e1f10d7955e54019dc
emirmoretti <emir.moretti@hotmail.com.ar>
2022-08-10 02:29:12 -0300

    getAll method added in comment controller

--- src/main/java/com/alkemy/ong/controller/CommentController.java
+++ src/main/java/com/alkemy/ong/controller/CommentController.java
@@ -1,16 +1,15 @@
 package com.alkemy.ong.controller;
 
 import com.alkemy.ong.dto.CommentDto;
+import com.alkemy.ong.dto.response.CommentResponseDTO;
 import com.alkemy.ong.service.ICommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/comments")
@@ -24,5 +23,14 @@ public class CommentController {
             @RequestBody CommentDto commentDto
     ){
         return new ResponseEntity<>(iCommentService.save(commentDto), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAll(){
+        List<CommentResponseDTO> response = iCommentService.getAllResponseDto();
+        if(response == null || response.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(iCommentService.getAllResponseDto());
     }
 }
