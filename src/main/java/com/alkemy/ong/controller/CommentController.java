4aae611ab02c067c8e372a89fa6791bdb3f966a9
emirmoretti <emir.moretti@hotmail.com.ar>
2022-08-11 15:34:07 -0300

    message error added

--- src/main/java/com/alkemy/ong/controller/CommentController.java
+++ src/main/java/com/alkemy/ong/controller/CommentController.java
@@ -4,18 +4,22 @@ import com.alkemy.ong.dto.CommentDto;
 import com.alkemy.ong.dto.response.CommentResponseDTO;
 import com.alkemy.ong.service.ICommentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Locale;
 
 @RestController
 @RequestMapping("/comments")
 public class CommentController {
 
     @Autowired private ICommentService iCommentService;
+    @Autowired
+    private MessageSource messageSource;
 
     @PostMapping
     public ResponseEntity<CommentDto> addComment(
@@ -29,7 +33,9 @@ public class CommentController {
     public ResponseEntity<?> getAll(){
         List<CommentResponseDTO> response = iCommentService.getAllResponseDto();
         if(response == null || response.isEmpty()){
-            return ResponseEntity.noContent().build();
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(messageSource.getMessage("error.commentList.empty", null, Locale.US));
         }
         return ResponseEntity.ok(iCommentService.getAllResponseDto());
     }
