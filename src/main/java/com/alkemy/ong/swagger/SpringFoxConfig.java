85327e5a7c08a3e49121f5afdc6c7db661010c5a
Jonathan Ramirez <52109489+JonathanRamirez1@users.noreply.github.com>
2022-08-14 15:24:25 -0500

    #OT248-88 - Agregar documentación a Testimonios

---
+++ src/main/java/com/alkemy/ong/swagger/SpringFoxConfig.java
@@ -0,0 +1,25 @@
+package com.alkemy.ong.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig {
+
+    @Bean
+    public Docket testimonialDocumentation() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.alkemy.ong.controller"))
+                .paths(PathSelectors.ant("/testimonials/*"))
+                //TODO AGREGA DOCUMENTACION A TODOS LOS CONTROLLERS CON SUS ENDPOINTS
+                /*.paths(PathSelectors.any())*/
+                .build();
+    }
+}
