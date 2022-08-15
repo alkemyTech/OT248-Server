c1ea3b95d9ee022ba2b8f8c68f0eff6d6a5391f1
emirmoretti <emir.moretti@hotmail.com.ar>
2022-08-15 18:38:29 -0300

    TestimonialPageResponse

--- src/main/java/com/alkemy/ong/service/impl/TestimonialServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/TestimonialServiceImpl.java
@@ -1,16 +1,24 @@
 package com.alkemy.ong.service.impl;
 
 import com.alkemy.ong.dto.TestimonialDto;
+import com.alkemy.ong.dto.response.TestimonialPageResponse;
 import com.alkemy.ong.exception.ResourceNotFoundException;
 import com.alkemy.ong.model.Testimonial;
 import com.alkemy.ong.repository.TestimonialRepository;
 import com.alkemy.ong.service.TestimonialService;
 import com.alkemy.ong.service.mapper.TestimonialMapper;
+
+import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -40,6 +48,36 @@ public class TestimonialServiceImpl implements TestimonialService {
         testimonialRepository.save(testimonial);
         Testimonial testimonialEntity = testimonialMapper.testimonialDtoToTestimonial(testimonialDto);
       return testimonialMapper.testimonialToTestimonialDto(testimonialEntity);
+    }
+
+    @Override
+    public TestimonialPageResponse getAll(int pageNo, int pageSize, String sortBy, String sortDir) {
+        Sort sort =  sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortBy).ascending()
+                : Sort.by(sortBy).descending();
+
+        Pageable pageable = PageRequest.of(pageNo, pageSize, sort);
+
+        Page<Testimonial> testimonials = testimonialRepository.findAll(pageable);
+
+        List<Testimonial> listOfTestomonials = testimonials.getContent();
+
+        List<TestimonialDto> content = listOfTestomonials
+                .stream()
+                .map(testimonial -> testimonialMapper.testimonialToTestimonialDto(testimonial))
+                .collect(Collectors.toList());
+
+        TestimonialPageResponse testimonialPageResponse = new TestimonialPageResponse();
+        testimonialPageResponse.setContent(content);
+        testimonialPageResponse.setPageNo(testimonials.getNumber());
+        testimonialPageResponse.setPageSize(testimonials.getSize());
+        testimonialPageResponse.setTotalElements(testimonials.getTotalElements());
+        testimonialPageResponse.setTotalPages(testimonials.getTotalPages());
+        testimonialPageResponse.setLastPage(testimonials.isLast());
+        testimonialPageResponse.setFirstPage(testimonials.isFirst());
+        testimonialPageResponse.setNextPage(testimonials.nextOrLastPageable());
+        testimonialPageResponse.setPreviousPage(testimonials.previousOrFirstPageable());
+
+        return testimonialPageResponse;
     }
 
     @Override
