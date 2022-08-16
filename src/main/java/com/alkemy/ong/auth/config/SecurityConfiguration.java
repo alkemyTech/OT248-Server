dcafee83cf20b7aedfca34a99da1b75d6a986503
Maxi <maxifop96@gmail.com>
2022-08-16 13:05:44 -0300

    Configured Swagger

--- src/main/java/com/alkemy/ong/auth/config/SecurityConfiguration.java
+++ src/main/java/com/alkemy/ong/auth/config/SecurityConfiguration.java
@@ -50,6 +50,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         httpSecurity.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/auth/*").permitAll()
+                .antMatchers("/v2/api-docs",
+                        "/configuration/ui",
+                        "/swagger-resources/**",
+                        "/configuration/security",
+                        "/swagger-ui.html",
+                        "/webjars/**").permitAll()
 
                 ////////////////////
                 //  Admin Routes  //
